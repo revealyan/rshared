efcbe3efc75f8f57206c227cf4ea98ed677f559c
revealyan <revealyan@gmail.com>
2026-09-06 16:55:55 +0300

    chore: переход на RShared.slnx, DependencyInjections удалён

--- RShared.DependencyInjections/IRegistry.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace RShared.DependencyInjections;
-
-public interface IRegistry
-{
-	public void RegisterServices(IServiceCollection services);
-}
